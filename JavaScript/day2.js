007b3edef49a19e3db9c2fcacc92c8085b506c74
Soham Acham <sohamacham20@gmail.com>
2026-08-18 08:19:46 +0530

    conversions

---
+++ JavaScript/day2.js
@@ -0,0 +1,27 @@
+
+let score =23;
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let score1 ="23";
+console.log(typeof score1)
+console.log(typeof (score1))
+
+let score2 = true;
+console.log(typeof score2)
+console.log(typeof (score2))
+
+let score3 = Number(null)
+console.log(typeof score3)
+console.log(typeof (score3))
+console.log(score3)
+
+let isboolean = Boolean(1)
+console.log(isboolean)
+let isboolean1 = Boolean("")
+console.log(isboolean1)
+
+let someNum= String(23)
+console.log(someNum)
+console.log(typeof someNum)
