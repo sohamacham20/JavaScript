64f6f60e9d6b0d33604cff6c35b952379d840717
Soham Acham <sohamacham20@gmail.com>
2026-08-18 07:52:16 +0530

    datatypes

---
+++ JavaScript/day1.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js as a newer version of js
+//alert(3+3)  // we are using the nodejs and note the browser so alert will not work here
+console.log(3+3)
+
+console.log("Hello, World!")// the above code will work but the readability is low , whenever we write the code the code the readability should be high
+
+//datatypes in js
+//number - range - 2 to power of 53
+//bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => variable declared but no value assigned
+// symbol => unique identifier
+
+console.log(typeof null)
