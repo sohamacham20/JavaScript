8b1348ce98a8af1466ae3e4690a0c178caaf49b2
Soham Acham <sohamacham20@gmail.com>
2026-08-19 07:41:28 +0530

    day3p1

---
+++ JavaScript/day3Operationa.js
@@ -0,0 +1,25 @@
+let val =12
+let negval = -val
+console.log(negval)
+// operations in javaScript
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log(1+2+"3")
+console.log("1"+2+3)
+
+console.log(+true);
+// console.log(true+); throws error
+console.log(+"")
