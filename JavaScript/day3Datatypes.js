23cb175c0cbce9f4dc2522cacee0d549aedd4170
Soham Acham <sohamacham20@gmail.com>
2026-08-24 10:03:12 +0530

    datatpe2

--- JavaScript/day3Datatypes.js
+++ JavaScript/day3Datatypes.js
@@ -26,3 +26,34 @@ const myfunction = function(){
     console.log("Hello, World!");
 }
 
+// *********************************************************************************************************
+
+// memory allocation in js
+// Stack and Heap Memory
+// stack stores the primitive datatypes
+// stack gives the copy of the primitive datatypes to the variable
+
+
+let myname="soham";
+yourname=myname;
+console.log(myname)
+console.log(yourname)
+yourname="yuktaa"
+console.log(yourname) // here the copy of the name was provided and not the reference so 
+// when we change the yourname it will not affect the myname
+
+//heap memory stores the reference datatypes
+//when we assign the reference datatypes to the variable it will give the reference of the variable to the other variable
+
+let userOne={
+    name:"soham",
+    age:22,
+    email:"soham@example.com"
+}
+
+let usertwo=userOne;
+
+usertwo.email="yuktaa@example.com"
+
+console.log(userOne.email) // here the reference of the userOne was provided 
+// to the usertwo so when we change the usertwo it will also affect the userOne
