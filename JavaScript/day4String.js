002e288608ad6f8f3ad5efe2ab49305089e62db6
Soham Acham <sohamacham20@gmail.com>
2026-08-27 10:10:58 +0530

    wed27Aug1

--- JavaScript/day4String.js
+++ JavaScript/day4String.js
@@ -11,3 +11,24 @@ console.log(gameName.toLowerCase());
 console.log(gameName.length);
 console.log(gameName[0]);
 console.log(gameName[gameName.length-1]);
+console.log(gameName.__proto__); 
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anStr= gameName.slice(-5,4);
+console.log(anStr);
+
+const str1 = "    Jai Jinendra    ";
+console.log(str1);
+console.log(str1.trim()); //trim is used to remove the whitespaces from the string
+
+const url = "https://www.youtube.com/%20watch?v=5qap5aO4i9A";
+console.log(url)
+console.log(url.includes('youtube'));
+console.log(url.replace('%20', '-'));
+
+console.log(gameName.split('-'))
